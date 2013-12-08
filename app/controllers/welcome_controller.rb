74df87acb266ca64fd865f1d9242666633a571ce
Jaclyn Jimenez <jaclynejimenez@gmail.com>
2013-12-08 15:34:09 -0500

    working version of site with nav

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,9 @@
+class WelcomeController < ApplicationController
+
+  def index
+  end
+  def work
+  end
+  def contact
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JaclynsPortfolio::Application.config.secret_token = 'b0f4e1548dd2ccd74d8435d0c1f2ebd8ccfc937df0ab4fca5e2cdefc9da5c904eb38227606bb3fab2b5f57bcbc9bbe64805e718beab76d48a8e581666c300700'
